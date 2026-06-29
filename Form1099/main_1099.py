57a5b053be895136cc4c5f582dfcadf64466d986
Suhas Modi <shmodi@zipai.com>
2026-06-29 18:52:27 +0530

    Cleaned code, removed extra comments

--- Form1099/main_1099.py
+++ Form1099/main_1099.py
@@ -5,13 +5,10 @@ from Form1099.div_1099 import extract_1099_div, save_results
 
 import logging
 import json
-import logging
 import os
 import boto3
 from botocore.exceptions import ClientError
 from pathlib import Path
-import base64
-import re
 from typing import Optional
 from datetime import datetime
 
@@ -28,11 +25,11 @@ logger = logging.getLogger("1099_extractor")
 
 AWS_REGION = "us-east-1"
 TEXTRACT_CLIENT = boto3.client("textract", region_name=AWS_REGION)
-BEDROCK_CLIENT = boto3.client("bedrock-runtime", region_name=AWS_REGION)
-BEDROCK_MODEL_ID = "amazon.nova-lite-v1:0"
+# BEDROCK_CLIENT = boto3.client("bedrock-runtime", region_name=AWS_REGION)
+# BEDROCK_MODEL_ID = "amazon.nova-lite-v1:0"
+
 
 def extract_1099_data(file_path):
-    # file_path=r"C:\Users\Lenovo\Downloads\f1099msc--2024-pages.pdf"
     with open(file_path, "rb") as f:
         document_bytes = f.read()
     try:
@@ -85,31 +82,3 @@ def extract_1099_data(file_path):
 
     return "INVALID 1099 FORM FOUND"
 
-
-# file_path=r"C:\Users\Lenovo\Desktop\ZIPAI_proj\FormFormats\1099\1099_MISC_1.pdf"
-# file_path=r"C:\Users\Lenovo\Desktop\ZIPAI_proj\FormFormats\1099\1099_DIV_1.pdf"
-# file_path=r"C:\Users\Lenovo\Desktop\ZIPAI_proj\FormFormats\1099\1099_NEC_1.pdf"
-# file_path=r"C:\Users\Lenovo\Desktop\ZIPAI_proj\FormFormats\1099\1099_INT_1.pdf"
-# file_path=r"C:\Users\Lenovo\Downloads\f1099msc--2024-pages.pdf"
-
-
-# form_name=extract_file_name(file_path)
-
-
-# match form_name:
-#     case "Form 1099-NEC":
-#         result=extract_1099_nec(file_path,use_textract=True)
-#         save_results(result, output_path="json/NEC1099_extracted_output.json")
-#     case "Form 1099-MISC":
-#         result=extract_1099_misc(file_path,use_textract=True)
-#         save_results(result, output_path="json/MISC1099_extracted_output.json")
-#     case "Form 1099-INT":
-#         result=extract_1099_int(file_path,use_textract=True)
-#         save_results(result, output_path="json/INT1099_extracted_output.json")
-#     case "Form 1099-DIV":
-#         result=extract_1099_div(file_path,use_textract=True)
-#         save_results(result, output_path="json/DIV1099_extracted_output.json")
-        
-
-#     case _:
-#         pass
